04f5a738a9b6fd35d2c88ee805b29f875e201515
vivquit-create <viv.quit@gmail.com>
2026-03-22 21:50:35 +0500

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,50 @@
+const mineflayer = require('mineflayer')
+
+function createBot() {
+  const bot = mineflayer.createBot({
+    host: 'titancraftsmp.mcsh.io',
+    port: 25565,
+    username: 'MR_BOT',
+    version: '1.21.4',
+    auth: 'offline'
+  })
+
+  bot.on('spawn', () => {
+    console.log('Bot spawned!')
+    
+    setInterval(() => {
+      bot.chat('/home')
+      console.log('Sent /home')
+    }, 30000)
+
+    setInterval(() => {
+      bot.setControlState('jump', true)
+      setTimeout(() => {
+        bot.setControlState('jump', false)
+      }, 500)
+      bot.setControlState('forward', true)
+      setTimeout(() => {
+        bot.setControlState('forward', false)
+      }, 1000)
+    }, 5000)
+  })
+
+  bot.on('kicked', (reason) => {
+    console.log('Kicked:', reason)
+    console.log('Reconnecting in 5 seconds...')
+    setTimeout(createBot, 5000)
+  })
+
+  bot.on('error', (err) => {
+    console.log('Error:', err)
+    console.log('Reconnecting in 5 seconds...')
+    setTimeout(createBot, 5000)
+  })
+
+  bot.on('end', () => {
+    console.log('Disconnected! Reconnecting in 5 seconds...')
+    setTimeout(createBot, 5000)
+  })
+}
+
+createBot()
